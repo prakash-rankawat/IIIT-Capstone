c01dcb91ca252e41bc308921a23346dcf7034e49
harshraham <harshrahamatkar@gmail.com>
2022-03-20 17:50:21 +0530

    added classification model

--- capstone/scraping/scraper.py
+++ capstone/scraping/scraper.py
@@ -24,9 +24,9 @@ def feature_extract(url,save_flag=True):
     source = requests.get(url).text
     soup = BeautifulSoup(source,"html.parser")
 
-    #to find the Stylesheets
-    #links = soup.find_all("link", {"rel":"stylesheet"})
-    #numCSS = len(links)
+    # to find the Stylesheets
+    # links = soup.find_all("link", {"rel":"stylesheet"})
+    # numCSS = len(links)
 
     # strip all script and style elements
     for script in soup(["script", "style"]):
@@ -47,19 +47,19 @@ def feature_extract(url,save_flag=True):
 
     # Text based features
     # token count
-    token_count = len(text)
+    tokens = len(text)
 
     # commas count
-    commas_count = len(re.findall(',', text))
+    commas = len(re.findall(',', text))
 
     # exclamations count
-    exclamations_count = len(re.findall('!', text))
+    exclamations = len(re.findall('!', text))
 
     # dots count
-    dots_count = len(re.findall('\.', text))
+    dots = len(re.findall('\.', text))
 
     # questions count
-    questions_count = len(re.findall("\?", text))
+    questions = len(re.findall("\?", text))
 
     # polarity
     text_blob = TextBlob(text)
@@ -73,12 +73,10 @@ def feature_extract(url,save_flag=True):
     for sentences_array in arr:
         sentences_list += list(filter(lambda x: len(x) > 0, sentences_array))
 
-
-    positive_sentences_count = 0
-    negative_sentences_count = 0
-    subjective_sentences_count = 0
-    objective_sentences_count = 0
-
+    positive_sentences = 0
+    negative_sentences = 0
+    subjective_sentences = 0
+    objective_sentences = 0
 
     for sentence in sentences_list:
         sent = TextBlob(sentence)
@@ -86,14 +84,14 @@ def feature_extract(url,save_flag=True):
         subjectivity = sent.sentiment.subjectivity
 
         if polarity > 0.0:
-            positive_sentences_count += 1
+            positive_sentences += 1
         else:
-            negative_sentences_count += 1
+            negative_sentences += 1
 
         if subjectivity >= 0.3:
-            subjective_sentences_count += 1
+            subjective_sentences += 1
         else:
-            objective_sentences_count += 1
+            objective_sentences += 1
 
     text_new = ""
     for word in text.split(" "):
@@ -102,13 +100,13 @@ def feature_extract(url,save_flag=True):
 
     # spelling errors count
     spell = SpellChecker()
-    spelling_errors_count = 0
+    spelling_errors = 0
     for word in text_new.split(" "):
         correct_word = spell.correction(word)
         if not word == correct_word:
-            spelling_errors_count += 1
+            spelling_errors += 1
 
-    #Entropy(text_complexity)
+    # Entropy(text_complexity)
     tokenizer = TweetTokenizer()
     tokens = tokenizer.tokenize((text))
     num_tokens = len(tokens)
@@ -130,9 +128,9 @@ def feature_extract(url,save_flag=True):
     # 'Adverb' : 'RB',
     # 'Determiner' : 'DT'
 
-    #JJ	adjective	'big'
-    #JJR	adjective, comparative	'bigger'
-    #JJS	adjective, superlative	'biggest'
+    # JJ	adjective	'big'
+    # JJR	adjective, comparative	'bigger'
+    # JJS	adjective, superlative	'biggest'
     # NN	noun, singular 'desk'
     # NNS	noun plural	'desks'
     # NNP	proper noun, singular	'Harrison'
@@ -147,70 +145,72 @@ def feature_extract(url,save_flag=True):
     # RBR	adverb, comparative	better
     # RBS	adverb, superlative	best
 
-
     # count number of nouns, verbs, adjectives, adverbs, determiners
     for sentence in sentences_list:
         text = nltk.word_tokenize(sentence)
         list_of_tags = nltk.pos_tag(text)
-        noun_count=0
-        verb_count=0
-        adj_count=0
-        adv_count=0
-        deter_count=0
+        NN = 0
+        VB = 0
+        JJ = 0
+        RB = 0
+        DT = 0
 
     for tag_tuple in list_of_tags:
         tag = tag_tuple[1]
-        if(tag in ['NN', 'NNS', 'NNP', 'NNPS']):
-            noun_count +=1
-        elif(tag in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']):
-            verb_count +=1
-        elif(tag in ['JJ', 'JJR', 'JJS']):
-            adj_count +=1
-        elif(tag in ['RB', 'RBR', 'RBS']):
-            adv_count +=1
-        elif(tag == 'DT'):
-            deter_count +=1
+        if (tag in ['NN', 'NNS', 'NNP', 'NNPS']):
+            NN += 1
+        elif (tag in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']):
+            VB += 1
+        elif (tag in ['JJ', 'JJR', 'JJS']):
+            JJ += 1
+        elif (tag in ['RB', 'RBR', 'RBS']):
+            RB += 1
+        elif (tag == 'DT'):
+            DT += 1
 
     # To get Alexa-Rank of URL
     alex_url = "https://www.alexa.com/siteinfo/" + url
-    alex_respone = r.get(alex_url) # get information from page
-    alex_soup = BeautifulSoup(alex_respone.content,'html.parser')
-    for match in alex_soup.find_all('span'): #remove all span tag
+    alex_respone = r.get(alex_url)  # get information from page
+    alex_soup = BeautifulSoup(alex_respone.content, 'html.parser')
+    for match in alex_soup.find_all('span'):  # remove all span tag
         match.unwrap()
-    global_rank = alex_soup.select('p.big.data') # select any p tag with big and data class
+    global_rank = alex_soup.select('p.big.data')  # select any p tag with big and data class
     global_rank = str(global_rank[0])
-    res = re.findall(r"([0-9,]{1,12})", global_rank) # find rank
-    alexa_rank=res[0]
+    res = re.findall(r"([0-9,]{1,12})", global_rank)  # find rank
+    alexa_rank = res[0]
 
     # To get domain name of a URL
     get_domain = urlparse(url).netloc
-    domain = '.'.join(get_domain.split('.')[-1:])
+    document_url_y = '.'.join(get_domain.split('.')[-1:])
 
-    if(save_flag == True):
+    if (save_flag == True):
         # open the file in the write mode
-        with open('url_features.csv', 'a',newline='') as csvfile:
+        with open('url_features.csv', 'a', newline='') as csvfile:
             # csv header
             #            header = ['token_count','commas_count','exclamations_count','dots_count','questions_count',
             #                       'polarity','positive_sentences_count','negative_sentences_count',
             #                       'subjective_sentences_count','objective_sentences_count',
             #                       'spelling_errors_count','text_complexity','smog',
             #                       'noun_count','verb_count','adj_count','deter_count']
-            data = [url,token_count,commas_count,exclamations_count,dots_count,questions_count,
-                    polarity,positive_sentences_count,negative_sentences_count,
-                    subjective_sentences_count,objective_sentences_count,
-                    spelling_errors_count,text_complexity,smog,
-                    noun_count,verb_count,adj_count,deter_count,alexa_rank,domain]
-
+            data = [
+            url, token,commas, exclamations, dots, questions,
+                polarity, positive_sentences, negative_sentences,
+                subjective_sentences, objective_sentences,
+                spelling_errors, text_complexity, smog,
+                NN, VB, JJ, RB, DT, alexa_rank, document_url_y
+            ]
             # create the csv writer
             writer = csv.writer(csvfile)
             # write header & a row to the csv file
             # writer.writerow(header)
             writer.writerow(data)
 
-    feature_list=[token_count,commas_count,exclamations_count,dots_count,questions_count,
-                  polarity,positive_sentences_count,negative_sentences_count,
-                  subjective_sentences_count,objective_sentences_count,
-                  spelling_errors_count,text_complexity,smog,
-                  noun_count,verb_count,adj_count,deter_count,alexa_rank,domain]
+    feature_list = [
+        tokens, commas, exclamations, dots, questions,
+        polarity, positive_sentences, negative_sentences,
+        subjective_sentences, objective_sentences,
+        spelling_errors, text_complexity, smog,
+        NN, VB, JJ, RB, DT, alexa_rank, document_url_y
+    ]
 
     return feature_list
